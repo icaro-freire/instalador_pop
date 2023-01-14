fd1ff3b92772351dbcbde6f14ae7cd9de0e847ab
icaro-freire <icarofreire7@gmail.com>
2023-01-13 23:40:54 -0300

    dev: retirando os comentários em main.sh

--- main.sh
+++ main.sh
@@ -31,22 +31,22 @@ echo
 echo 
 echo "***** Iniciando a instalação *************************************************"
 echo 
-#./scripts_bash/01_instalarGit.sh
-#./scripts_bash/02_instalarApt.sh
-#./scripts_bash/03_instalarSl.sh
-#./scripts_bash/04_instalarFontes.sh
-#./scripts_bash/05_moverScripts.sh
-#./scripts_bash/06_instalarLua.sh
-#./scripts_bash/07_instalarLaTeX.sh 
-#./scripts_bash/08_instalarNeovim.sh
-#./scripts_bash/09_configurarNeovim.sh
-#./scripts_bash/10_instalarR.sh
-#./scripts_bash/11_instalarRStudio.sh
+./scripts_bash/01_instalarGit.sh
+./scripts_bash/02_instalarApt.sh
+./scripts_bash/03_instalarSl.sh
+./scripts_bash/04_instalarFontes.sh
+./scripts_bash/05_moverScripts.sh
+./scripts_bash/06_instalarLua.sh
+./scripts_bash/07_instalarLaTeX.sh 
+./scripts_bash/08_instalarNeovim.sh
+./scripts_bash/09_configurarNeovim.sh
+./scripts_bash/10_instalarR.sh
+./scripts_bash/11_instalarRStudio.sh
 ./scripts_bash/12_instalarPacotesR.sh
-#./scripts_bash/13_instalarWXMaxima.sh 
-#./scripts_bash/14_instalarTor.sh
-#./scripts_bash/15_instalarFlatpak.sh
-#./scripts_bash/16_instalarOhMyZsh.sh
+./scripts_bash/13_instalarWXMaxima.sh 
+./scripts_bash/14_instalarTor.sh
+./scripts_bash/15_instalarFlatpak.sh
+./scripts_bash/16_instalarOhMyZsh.sh
 echo
 echo "***** FIM DAS CONFIGURAÇÕES **************************************************"
 echo 
@@ -57,7 +57,7 @@ echo "***** Iniciando limpeza **************************************************
 echo 
 echo "* removendo diretório temporário..."
 echo 
-#sudo rm "$TMP_INSTALACAO" -r 
+sudo rm "$TMP_INSTALACAO" -r 
 echo 
 echo "* atualizações e remoção de arquivos desnecessários..."
 echo 
